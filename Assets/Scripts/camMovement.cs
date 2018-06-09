c24e16c7f73eaa6a331058a712741fb653b9d44c
blackbird-91 <39198232+blackbird-91@users.noreply.github.com>
2018-06-09 12:22:58 +0530

    Fixed jump animation with collision

--- Assets/Scripts/camMovement.cs
+++ Assets/Scripts/camMovement.cs
@@ -6,10 +6,14 @@ public class camMovement : MonoBehaviour {
 
 	public Transform target;
 	public Transform cameraTracker;
+	public Transform player;
 	public float offsetBehindPlayer;
 	public float offsetAbovePlayer;
 	public Vector3 angularOffset;
 	public float smoothFactor;
+	public float dampTime;
+	Vector3 camVelocity;
+
 	Vector3 targetPosition;
 
 	public void LateUpdate()
@@ -20,8 +24,21 @@ public class camMovement : MonoBehaviour {
 		targetPosition = cameraTracker.position + 
 						cameraTracker.up * offsetAbovePlayer - viewVector * offsetBehindPlayer;
 
-		target.position = Vector3.Lerp (target.position, targetPosition, Time.deltaTime * smoothFactor);
+//		target.position = Vector3.Lerp (target.position, targetPosition, Time.deltaTime * smoothFactor);
+		target.position = Vector3.SmoothDamp (target.position, targetPosition, ref camVelocity, dampTime);
 		//target.eulerAngles = cameraTracker.transform.eulerAngles + angularOffset;
 		target.LookAt(cameraTracker);
+
+
+//		#region playerLookat
+//		// camera look at using the players position instead of camTarget gameobject
+//		Vector3 offset = player.position + (Vector3.up * 1.8f);
+//		Vector3 viewVector = offset - target.position;
+//		viewVector.y = 0f;
+//		viewVector.Normalize();
+//		targetPosition = offset + player.up * offsetAbovePlayer - viewVector * offsetBehindPlayer;
+//		target.position = Vector3.SmoothDamp(target.position, targetPosition, ref camVelocity, dampTime);
+//		target.LookAt(offset);
+//		#endregion
 	}
 }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -15,15 +15,57 @@ public class movement : MonoBehaviour {
 	private Transform camFollow;
 	[SerializeField]
 	private Transform cameraTransform;
+	[SerializeField]
+	private Rigidbody erikaBody;
+	[SerializeField]
+	private float fallMultiplier = 2.5f;
+	[SerializeField]
+	private float jumpMultiplier = 1.0f;
+	[SerializeField]
+	private float jumpDistance = 5.0f;
+
+
+	private float rotationDegreesPerSecond = 120.0f;
+	private Vector3 MoveDirection = Vector3.zero;
+	private bool isCharGrounded = true;
+	private CapsuleCollider erikaCollider;
+//	private Vector3 colHeight;
+	private float colHeight;
+	float oldY = 0.0f;
 
 	void Start () {
+		erikaCollider = erikaBody.transform.GetComponent<CapsuleCollider>();
+//		colHeight = erikaCollider.size;
 	}
-	
+
+	void FixedUpdate()
+	{
+		Quaternion charRotation = Quaternion.FromToRotation (charPosition.forward, MoveDirection);
+		Quaternion newRotation = charPosition.rotation * charRotation;
+		charPosition.rotation = Quaternion.Slerp(charPosition.rotation, newRotation, 3.0f * Time.deltaTime);
+
+		if (isInJump ()) {
+//						colHeight.y = erikaAnimController.GetFloat("colliderHeight");
+			if (!erikaAnimController.IsInTransition(0)){
+				oldY = erikaBody.transform.position.y;
+//				erikaBody.transform.Translate (Vector3.up * erikaAnimController.GetFloat ("JumpCurve") * jumpMultiplier);
+				erikaBody.transform.Translate (Vector3.forward * jumpDistance * Time.deltaTime);
+
+				colHeight = erikaAnimController.GetFloat ("colliderHeight");
+				erikaCollider.height = colHeight;
+
+//				cameraTransform.Translate (Vector3.up * (erikaBody.transform.position.y - oldY));
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
 		currentSpeed = Input.GetAxis("Vertical");
 		currentDirection = Input.GetAxis ("Horizontal");
+
+		#region forwardTurn
 		float speed = Mathf.Abs(currentSpeed) + Mathf.Abs(currentDirection);
 		speed = Mathf.Clamp (speed, -1.0f, 1.0f);
 		erikaAnimController.SetFloat ("Speed", speed);
@@ -36,11 +78,38 @@ public class movement : MonoBehaviour {
 		Vector3 camDirection = cameraTransform.forward;
 		camDirection.y = 0.0f;
 		Quaternion refShift = Quaternion.FromToRotation (Vector3.forward, camDirection);
-		Vector3 MoveDirection = refShift * stickDirection;
-
-		Quaternion charRotation = Quaternion.FromToRotation (charPosition.forward, MoveDirection);
-		charPosition.rotation = Quaternion.Slerp(charPosition.rotation, charPosition.rotation * charRotation, 3.0f * Time.deltaTime);
+		MoveDirection = refShift * stickDirection;
 
 		Debug.DrawRay (new Vector3 (charPosition.position.x, charPosition.position.y + 2.0f, charPosition.position.z), MoveDirection, Color.green);
+		#endregion
+
+		if (Input.GetButtonDown ("Jump")) {
+			erikaAnimController.SetTrigger ("Jump");
+		}
+//		print (erikaBody.velocity);
+
+//		isCharGrounded = isGrounded();
 	}
+
+//	bool isGrounded()
+//	{
+//		Ray downRay = new Ray (erikaBody.transform.position, -erikaBody.transform.up);
+//		RaycastHit hit;
+//		if (Physics.Raycast (downRay, out hit, 0.2f)) {
+//			if (hit.collider.tag == "Floor")
+//				return true;
+//		}
+//		return false;
+//	}
+
+	public bool isInJump()
+	{
+		return (erikaAnimController.GetCurrentAnimatorStateInfo (0).IsName ("Base Layer.Jump"));
+	}
+
+//	void LateUpdate()
+//	{
+//		if (isInJump())
+//			cameraTransform.Translate (Vector3.up * (erikaBody.transform.position.y - oldY));
+//	}
 }
